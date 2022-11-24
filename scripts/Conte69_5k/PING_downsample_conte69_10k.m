be7d625b1f96870675f7bfdc2b92d3a55c909fef
Serena Fett <90796533+sfett@users.noreply.github.com>
2022-11-24 17:31:31 -0500

    Add files via upload

---
+++ scripts/Conte69_5k/PING_downsample_conte69_10k.m
@@ -0,0 +1,46 @@
+%% Downsample  timeseries from 32k to conte69 5k
+
+clear
+clc
+
+root_path = '/data/mica3/BIDS_PING/derivatives/micapipe/' ;
+
+%load subject list
+subj = '/data_/mica1/03_projects/serena/scripts/PING/PING_final_cohort.csv';
+S = readtable(subj);
+sID = S.Subjects;
+
+% Get mapping 32k -> 10k
+load('/data_/mica1/03_projects/valeria/micasoft/parcellations/fs_LR-conte69/fsaverage.midthickness_mni_32k_fs_LR.mat');
+C69 = G; clear G;
+
+load('/data_/mica1/03_projects/valeria/micasoft/parcellations/conte69_10k/conte69_10k.mat');
+C69_10k = struct('coord', coord, 'tri', int32(tri));
+downSampIdx = idx;
+
+g = gifti('/data_/mica1/03_projects/valeria/micasoft/parcellations/conte69_10k/conte69_10k.surf.gii');
+
+
+for sub = 1:length(sID)
+    sub_path = fullfile(root_path,['sub-' sID{sub}], 'ses-01','func','surfaces');
+    sub_file = fullfile(sub_path, ['sub-' sID{sub} '_ses-01_rsfmri_space-conte69-32k_desc-timeseries_clean.txt']) ;
+    TS_clean = load(sub_file);
+    
+    %remove first 48 vertices 
+    TS_clean(:,1:48)=[];
+
+    % Downsample to 10k
+    for iter = 1:length(downSampIdx)
+        this_idx = downSampIdx(iter);
+        TS_clean_10k(:,iter) = TS_clean(:,this_idx);
+        clear this_idx
+    end
+
+    % Create a table with the data and variable namesp
+    T = table(TS_clean_10k);
+    
+    % Write data to text file
+    writetable(T,fullfile(sub_path, ['sub-' sID{sub} '_ses-01_rsfmri_space-conte69-10k_desc-timeseries_clean.txt']),'WriteVariableNames',0);
+    
+    clear T TS_clean TS_clean_10k sub_path sub_file
+end 
