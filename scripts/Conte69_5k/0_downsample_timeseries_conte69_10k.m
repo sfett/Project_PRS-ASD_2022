ea1a2f75e8ebcbdfd9621ef427032ebe8cca27fa
Serena Fett <90796533+sfett@users.noreply.github.com>
2022-11-28 15:59:50 -0500

    Update 0_downsample_timeseries_conte69_10k.m

--- scripts/Conte69_5k/0_downsample_timeseries_conte69_10k.m
+++ scripts/Conte69_5k/0_downsample_timeseries_conte69_10k.m
@@ -1,26 +1,35 @@
-%% Script used to downsample timeseries data from higher resolution (i.e., 32k) to lower (i.e.,5k) in conte69 space. 
-  
-  
-root_path = '/data/mica3/BIDS_PING/derivatives/micapipe/' ;
+%% This MATLAB script is used to downsample timeseries data from higher resolution (i.e., 32k) to lower (i.e.,5k) in conte69 space. 
+%% Downsampling timeseries data is often used for purposes of computational efficiency, especially with large datasets. 
 
-%load subject list
+root_path = '/data/mica3/BIDS_PING/derivatives/micapipe/' ;
+surf_path = '/github/Project_PRS-ASD_2022/surfaces/' ; 
+
+% file name of inital resolution 
+input_ts_name = '_ses-01_rsfmri_space-conte69-32k_desc-timeseries_clean.txt' 
+
+% file name of target downsample resolution
+output_ts_name = '_ses-01_rsfmri_space-conte69-10k_desc-timeseries_clean.txt'
+
+% Load subject list
 subj = %uplaod csv file of subjects
 S = readtable(subj);
 sID = S.Subjects;
 
 % Get mapping 32k -> 10k
-load('/data_/mica1/03_projects/valeria/micasoft/parcellations/fs_LR-conte69/fsaverage.midthickness_mni_32k_fs_LR.mat');
+load(fullfile(surf_path, 'fsaverage.midthickness_mni_32k_fs_LR.mat');
 C69 = G; clear G;
 
-load('/data_/mica1/03_projects/valeria/micasoft/parcellations/conte69_10k/conte69_10k.mat');
+load(fullfile(surf_path, 'conte69_10k.mat');
+
 C69_10k = struct('coord', coord, 'tri', int32(tri));
 downSampIdx = idx;
 
-g = gifti('/data_/mica1/03_projects/valeria/micasoft/parcellations/conte69_10k/conte69_10k.surf.gii');
+g = gifti(fullfile(surf_path, 'conte69_10k.surf.gii');
 
+% Iterate for all subjects
 for sub = 1:length(sID)
     sub_path = fullfile(root_path,['sub-' sID{sub}], 'ses-01','func','surfaces');
-    sub_file = fullfile(sub_path, ['sub-' sID{sub} '_ses-01_rsfmri_space-conte69-32k_desc-timeseries_clean.txt']) ;
+    sub_file = fullfile(sub_path, ['sub-' sID{sub}, input_ts_name]) ;
     TS_clean = load(sub_file);
     
     %remove first 48 vertices 
@@ -37,7 +46,7 @@ for sub = 1:length(sID)
     T = table(TS_clean_10k);
     
     % Write data to text file
-    writetable(T,fullfile(sub_path, ['sub-' sID{sub} '_ses-01_rsfmri_space-conte69-10k_desc-timeseries_clean.txt']),'WriteVariableNames',0);
+    writetable(T,fullfile(sub_path, ['sub-' sID{sub}, output_ts_name]),'WriteVariableNames',0);
     
     clear T TS_clean TS_clean_10k sub_path sub_file
 end 
